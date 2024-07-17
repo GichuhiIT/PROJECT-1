d0b7b8f447d37ad9b0e4dd764ec6617c91a9b18f
GichuhiIT <davidgichuhi007@gmail.com>
2024-07-17 14:32:47 +0300

    Add files via upload

---
+++ LIBRARYMS.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAuthor = New System.Windows.Forms.Label()
+        Me.lblYearPublished = New System.Windows.Forms.Label()
+        Me.lblGenre = New System.Windows.Forms.Label()
+        Me.txtGenre = New System.Windows.Forms.TextBox()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtAuthor = New System.Windows.Forms.TextBox()
+        Me.txtYearPublished = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 16)
+        Me.Label1.TabIndex = 0
+        '
+        'lblAuthor
+        '
+        Me.lblAuthor.AutoSize = True
+        Me.lblAuthor.Location = New System.Drawing.Point(0, 94)
+        Me.lblAuthor.Name = "lblAuthor"
+        Me.lblAuthor.Size = New System.Drawing.Size(45, 16)
+        Me.lblAuthor.TabIndex = 1
+        Me.lblAuthor.Text = "Author"
+        '
+        'lblYearPublished
+        '
+        Me.lblYearPublished.AutoSize = True
+        Me.lblYearPublished.Location = New System.Drawing.Point(0, 199)
+        Me.lblYearPublished.Name = "lblYearPublished"
+        Me.lblYearPublished.Size = New System.Drawing.Size(96, 16)
+        Me.lblYearPublished.TabIndex = 2
+        Me.lblYearPublished.Text = "YearPublished"
+        '
+        'lblGenre
+        '
+        Me.lblGenre.AutoSize = True
+        Me.lblGenre.Location = New System.Drawing.Point(0, 310)
+        Me.lblGenre.Name = "lblGenre"
+        Me.lblGenre.Size = New System.Drawing.Size(44, 16)
+        Me.lblGenre.TabIndex = 3
+        Me.lblGenre.Text = "Genre"
+        '
+        'txtGenre
+        '
+        Me.txtGenre.Location = New System.Drawing.Point(162, 304)
+        Me.txtGenre.Name = "txtGenre"
+        Me.txtGenre.Size = New System.Drawing.Size(100, 22)
+        Me.txtGenre.TabIndex = 4
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(162, 0)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(100, 22)
+        Me.txtTitle.TabIndex = 5
+        '
+        'txtAuthor
+        '
+        Me.txtAuthor.Location = New System.Drawing.Point(162, 88)
+        Me.txtAuthor.Name = "txtAuthor"
+        Me.txtAuthor.Size = New System.Drawing.Size(100, 22)
+        Me.txtAuthor.TabIndex = 6
+        '
+        'txtYearPublished
+        '
+        Me.txtYearPublished.Location = New System.Drawing.Point(162, 199)
+        Me.txtYearPublished.Name = "txtYearPublished"
+        Me.txtYearPublished.Size = New System.Drawing.Size(100, 22)
+        Me.txtYearPublished.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(372, 0)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(108, 35)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(372, 87)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(108, 39)
+        Me.btnUpdate.TabIndex = 9
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(372, 196)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(108, 38)
+        Me.btnDelete.TabIndex = 10
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(372, 304)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(108, 41)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBooks.Location = New System.Drawing.Point(676, 0)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.RowHeadersWidth = 51
+        Me.dgvBooks.RowTemplate.Height = 24
+        Me.dgvBooks.Size = New System.Drawing.Size(355, 345)
+        Me.dgvBooks.TabIndex = 12
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(0, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(33, 16)
+        Me.lblTitle.TabIndex = 13
+        Me.lblTitle.Text = "Title"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1043, 450)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtYearPublished)
+        Me.Controls.Add(Me.txtAuthor)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.txtGenre)
+        Me.Controls.Add(Me.lblGenre)
+        Me.Controls.Add(Me.lblYearPublished)
+        Me.Controls.Add(Me.lblAuthor)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblYearPublished As Label
+    Friend WithEvents lblGenre As Label
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents dgvBooks As DataGridView
+    Friend WithEvents lblTitle As Label
+End Class
